cb117a08246e4ab6f6f3ed8344ea49349cd88cca
yli-shg <yli@solitaryhillgames.com>
2024-12-19 19:17:26 +0000

    refined code and add example cases

--- summarizer.py
+++ summarizer.py
@@ -6,9 +6,10 @@ Created on Mon Dec 16 10:09:48 2024
 """
 
 import pickle
+import random
 import pandas as pd
 from rules import temp_rules
-from utilities.utilities import random_cut_in_time
+from utilities.utilities import random_cut_in_time, plot_records
 
 
 if __name__ == '__main__':
@@ -25,12 +26,18 @@ if __name__ == '__main__':
     # Example usage
     # Case 1: Febrile at admission, then settled
     case_1 = patients_with_long_febrile_period[88]
+    case_1_example_cut_in = pd.Timestamp('2024-03-24 01:50:53.786380805')
     
+    # Case 2: long in_patient record,long afebrile interval
+    case_2 = patients_with_long_febrile_period[0]
+    case_2_example_cut_in = pd.Timestamp('2023-12-16 09:00:00')
 
-    case = case_1
+    case = random.choice(sign_records)
     cut_in_time = random_cut_in_time(case)
+    #cut_in_time = case_1_example_cut_in
     summary = temp_rules.summarize_temperature_vitals(case, cut_in_time)
     print(f"Patient {case['patientID']}: {summary}")
+    plot_records(data=case, cut_in_time=cut_in_time)
     # Set a cut-in time
 
 
@@ -54,7 +61,7 @@ if __name__ == '__main__':
 
     cut_in_time = "2023-07-14 18:40:00"
     cut_in_time = pd.Timestamp(cut_in_time)
-    summary = temp_rules.summarize_temperature_vitals(patient_records, cut_in_time)
-    print(f"Patient {patient_records['patientID']}: {summary}")
+    summary = temp_rules.summarize_temperature_vitals(patient_records[0], cut_in_time)
+    print(f"Patient {patient_records[0]['patientID']}: {summary}")
         
     
